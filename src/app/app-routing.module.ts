c71e3528a8cbe9063dbd13c49ae1d961505073b8
onotaizee@gmail.com <onotaizee@gmail.com>
2023-07-17 15:25:35 +0100

    feat: speaker registration page feat: speaker registration page responsiveness

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { FaqComponent } from './pages/faq/faq.component';
+import { SpeakerComponent } from './pages/speaker/speaker.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent, title: "KINFLIP"},
   {path: 'faq', component: FaqComponent, title: "FAQ - KINFLIP"},
+  {path: 'speaker', component: SpeakerComponent, title: "Register - KINFLIP"},
   { path: '**', component: HomeComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { HomeComponent } from './pages/home/home.component';
 import { FaqComponent } from './pages/faq/faq.component';
+import { SpeakerComponent } from './pages/speaker/speaker.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FaqComponent,
+    SpeakerComponent,
   ],
   imports: [
     BrowserModule,
